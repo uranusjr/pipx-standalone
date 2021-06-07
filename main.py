f6a66dbd232cbb1194c0694e8dcbca36f239a39e
Tzu-ping Chung <uranusjr@gmail.com>
2021-06-07 21:30:34 +0800

    0.16.3

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import urllib.request
 import zipfile
 
 
-PIPX_VERSION = "0.15.6.0"
+PIPX_VERSION = "0.16.3"
 
 # Used to release manifest bug fixes without incrementing pipx version.
 MANIFEST_BUILD_NUMBER = 0
 
-PYTHON_EMBED_VERSION = "3.9.1"
+PYTHON_EMBED_VERSION = "3.9.5"
 
 PYTHON_EMBED_URL_TEMPLATE = (
     "https://www.python.org/ftp/python/{0}/python-{0}-embed-{1}.zip"
